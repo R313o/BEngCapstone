9c5ec583bf4a0716f12d5eb5525b128dd089f73a
Spencer Arnold <128935937+Spencer-Arnold1@users.noreply.github.com>
2025-04-29 15:22:32 -0700

    fixed DC offsets in pipe

--- STM32H743-DSP/Core/Src/pipe.c
+++ STM32H743-DSP/Core/Src/pipe.c
@@ -10,11 +10,15 @@
 static void pipe_ADC_HalfComplete(pipe *self, const volatile uint16_t *adcInput)
 {
     uint16_t i;
+    //load values from adc dma buffer
     for (i = 0; i < BUFFER_SIZE; i++)
     {
         self->inBuffer1[i] = (float32_t)adcInput[i];
     }
+    //convert values from quantized value to voltage range float
     arm_scale_f32(self->inBuffer1, ADC_BITS2VOLTS, self->inBuffer1, BUFFER_SIZE);
+    //remove DC offset
+    arm_offset_f32(self->inBuffer1, -1.65f, self->inBuffer1, BUFFER_SIZE);
     self->inBuffer  = self->inBuffer1;
     self->outBuffer = self->outBuffer1;
     self->ppState   = 1;
@@ -24,11 +28,15 @@ static void pipe_ADC_HalfComplete(pipe *self, const volatile uint16_t *adcInput)
 static void pipe_ADC_Complete(pipe *self, const volatile uint16_t *adcInput)
 {
     uint16_t i;
+    //load values from adc dma buffer
     for (i = 0; i < BUFFER_SIZE; i++)
     {
         self->inBuffer2[i] = (float32_t)adcInput[BUFFER_SIZE + i];
     }
+    //convert values from quantized value to voltage range float
     arm_scale_f32(self->inBuffer2, ADC_BITS2VOLTS, self->inBuffer2, BUFFER_SIZE);
+    //remove DC offset
+    arm_offset_f32(self->inBuffer2, -1.65f, self->inBuffer2, BUFFER_SIZE);
     self->inBuffer  = self->inBuffer2;
     self->outBuffer = self->outBuffer2;
     self->ppState   = 0;
@@ -60,9 +68,11 @@ static void pipe_updateDACOutput(pipe *self, volatile uint16_t *dacBuffer)
     uint16_t i;
 
     uint8_t half = (self->ppState == 0) ? 1 : 0;
-
+    //add dc offset
+    arm_offset_f32(self->inBuffer2, 1.65f, self->inBuffer2, BUFFER_SIZE);
+    //convert from voltage range to quantized dac value
     arm_scale_f32(self->outBuffer, DAC_VOLTS2BITS, self->outBuffer, BUFFER_SIZE);
-
+    //load dac dma buffer from output buffer
     for (i = 0; i < BUFFER_SIZE; i++)
     {
         dacBuffer[BUFFER_SIZE * half + i] = (uint16_t)self->outBuffer[i];
